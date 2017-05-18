142835284f3698bc0bbcb931d4117414c8fac899
Michael Balagna <lunchmeat13@gmail.com>
2017-05-18 14:08:57 -0600

    Add click functions for jabs and tilts

--- app.js
+++ app.js
@@ -2,37 +2,82 @@ $(appReady);
 
 function appReady(){
   var characterArr = []
-  //Get character id and attack array
-$('.DrMario, .Mario, .Luigi, .Bowser, .Peach, .Yoshi, .DonkeyKong, .CaptainFlacon, .Ganon').click(function() {
+  //Get character id and attack array from first row
+$('.DrMario, .Mario, .Luigi, .Bowser, .Peach, .Yoshi, .DonkeyKong, .CaptainFlacon, .Ganon, .fox').click(function() {
   $.get('https://cors-anywhere.herokuapp.com/http://www.smashlounge.com/api/attack/char/'+this.id).then(function(response){
     console.log(response);
-    characterArr.slice(0).push(response);
+    characterArr.splice(0)
+    characterArr.push(response);
     console.log(characterArr)
     ;
   });
 })
 
+//click on Jab 1 and get jab frames
+$(".jab1").click(function(){
+  $(".totalframes").empty().append("Total Frames: " + characterArr["0"].attacks.jab["0"].total_frames+ "  Active Frames: " + characterArr[0].attacks.jab[0].hit_start + "-" +characterArr[0].attacks.jab[0].hit_end);
+  $(".card-title").empty().append("Frame Data for " + $(this).text())
+  $(".gifattack").attr("src", "https://smashboards.com/proxy.php?image=http%3A%2F%2Fi882.photobucket.com%2Falbums%2Fac30%2FX1-12%2FFoxJab.gif%3Ft%3D1282910671&hash=26b37c01a59c854fece263e44035a494")
+  // $(".activeframes").append("<p>Active Frames: " + foxArr.hit_start + "-" +foxArr.hit_end+ "<p>" )
+  }
+)
+
+$(".jab2").click(function(){
+  $(".totalframes").empty().append("Total Frames: " + characterArr["0"].attacks.jab["1"].total_frames+ "  Active Frames: " + characterArr[0].attacks.jab[1].active_start + "-" +characterArr[0].attacks.jab[1].active_end);
+  $(".card-title").empty().append("Frame Data for " + $(this).text())
+  // $(".gifattack").attr("src", "https://smashboards.com/proxy.php?image=http%3A%2F%2Fi882.photobucket.com%2Falbums%2Fac30%2FX1-12%2FFoxJab.gif%3Ft%3D1282910671&hash=26b37c01a59c854fece263e44035a494")
+  // $(".activeframes").append("<p>Active Frames: " + foxArr.hit_start + "-" +foxArr.hit_end+ "<p>" )
+  }
+)
+
+$(".jab3").click(function(){
+  $(".totalframes").empty().append("Total Frames: " + characterArr["0"].attacks.jab["2"].total_frames+ "  Active Frames: " + characterArr[0].attacks.jab[2].hit_start + "-" +characterArr[0].attacks.jab[2].hit_end);
+  $(".card-title").empty().append("Frame Data for" + $(this).text())
+})
+
+$(".ftilt").click(function(){
+  $(".totalframes").empty().append("Total Frames: " + characterArr["0"].attacks.tilt[2].total_frames+ "  Active Frames: " + characterArr["0"].attacks.tilt[2].hit_start + "-" +characterArr["0"].attacks.tilt[2].hit_end);
+  $(".card-title").empty().append("Frame Data for " + $(this).text())
+  }
+)
+
+$(".uptilt").click(function(){
+  $(".totalframes").empty().append("Total Frames: " + characterArr["0"].attacks.tilt[3].total_frames+ "  Active Frames: " + characterArr["0"].attacks.tilt[4].hit_start + "-" +characterArr["0"].attacks.tilt[4].hit_end);
+  $(".card-title").empty().append("Frame Data for " + $(this).text())
+  }
+)
+
+$(".dtilt").click(function(){
+  $(".totalframes").empty().append("Total Frames: " + characterArr["0"].attacks.tilt[2].total_frames+ "  Active Frames: " + characterArr["0"].attacks.tilt[3].hit_start + "-" +characterArr["0"].attacks.tilt[3].hit_end);
+  $(".card-title").empty().append("Frame Data for " + $(this).text())
+  }
+)
+
+
+
+//This bracket is appReadybracket!
+}
 
 
   //click fox, get fox attack data, put into var.
-  var foxArr = []
-  $(".fox").click(function() {
-
-    $.get('https://cors-anywhere.herokuapp.com/http://www.smashlounge.com/api/attack/char/6')
-      .then(function(response){
-        console.log(response);
-          foxArr.push(response);
-         console.log(foxArr);
-    })
-  })
-  $(".jab1").click(function(){
-    $(".totalframes").empty().append("Total Frames: " + foxArr["0"].attacks.jab["0"].total_frames+ "  Active Frames: " + foxArr[0].attacks.jab[0].hit_start + "-" +foxArr[0].attacks.jab[0].hit_end);
-    $(".card-title").empty().append("Frame Data for Jab 1")
-    $(".gifattack").attr("src", "https://smashboards.com/proxy.php?image=http%3A%2F%2Fi882.photobucket.com%2Falbums%2Fac30%2FX1-12%2FFoxJab.gif%3Ft%3D1282910671&hash=26b37c01a59c854fece263e44035a494")
-    // $(".activeframes").append("<p>Active Frames: " + foxArr.hit_start + "-" +foxArr.hit_end+ "<p>" )
-    }
-  )
-}
+//   var foxArr = []
+//   $(".fox").click(function() {
+//
+//     $.get('https://cors-anywhere.herokuapp.com/http://www.smashlounge.com/api/attack/char/6')
+//       .then(function(response){
+//         console.log(response);
+//           foxArr.push(response);
+//          console.log(foxArr);
+//     })
+//   })
+//   $(".jab1").click(function(){
+//     $(".totalframes").empty().append("Total Frames: " + foxArr["0"].attacks.jab["0"].total_frames+ "  Active Frames: " + foxArr[0].attacks.jab[0].hit_start + "-" +foxArr[0].attacks.jab[0].hit_end);
+//     $(".card-title").empty().append("Frame Data for Jab 1")
+//     $(".gifattack").attr("src", "https://smashboards.com/proxy.php?image=http%3A%2F%2Fi882.photobucket.com%2Falbums%2Fac30%2FX1-12%2FFoxJab.gif%3Ft%3D1282910671&hash=26b37c01a59c854fece263e44035a494")
+//     // $(".activeframes").append("<p>Active Frames: " + foxArr.hit_start + "-" +foxArr.hit_end+ "<p>" )
+//     }
+//   )
+// }
 
 // Gets character's attack info based on id  6= "fox"
   // $.get('https://cors-anywhere.herokuapp.com/http://www.smashlounge.com/api/attack/char/6')
