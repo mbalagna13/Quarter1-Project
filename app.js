063f7c19680bf810c13d5b0b6f323d8c037b1838
Michael Balagna <lunchmeat13@gmail.com>
2017-05-19 08:57:11 -0600

    add gifs to click functions

--- app.js
+++ app.js
@@ -1,55 +1,88 @@
 $(appReady);
 
 function appReady(){
-  var characterArr = []
+  var characterArr = [];
+  var characterNameArr = [];
+  var charactergif = [];
+  var characterStr = ""
   //Get character id and attack array from first row
 $('.DrMario, .Mario, .Luigi, .Bowser, .Peach, .Yoshi, .DonkeyKong, .CaptainFlacon, .Ganon, .fox').click(function() {
   $.get('https://cors-anywhere.herokuapp.com/http://www.smashlounge.com/api/attack/char/'+this.id).then(function(response){
-    console.log(response);
+
+    // console.log(response);
+
     characterArr.splice(0)
     characterArr.push(response);
-    console.log(characterArr)
-    ;
+    characterNameArr = characterArr[0].charname;
+    console.log(characterArr);
+    // characterName = characterName[0].toString();
+  characterStr = Object.values(characterNameArr);
+  characterStr = characterStr.join("").toLowerCase().toString();
+  console.log(characterStr);
+  $.get('https://api.myjson.com/bins/dqybh').then(function(response){
+    console.log(response.fox);
+    charactergif = response[characterStr]
+
+    // console.log(charactergif[characterStr]);
+    console.log(charactergif.jabs["0"].gif);
+
   });
+  // $.get('https://api.myjson.com/bins/dqybh').then(function(response){
+  //   console.log(response.fox);
+  //   charactergif = response
+  //
+  //   console.log(charactergif[characterStr]);
+  //   console.log(charactergif);
+  //   console.log(char);
+  // })
+  // $.get("https://api.myjson.com/bins/dqybh").then(function(response){
+  //   console.log(response.responseJSON);
+  // })
 })
 
 //click on Jab 1 and get jab frames
 $(".jab1").click(function(){
-  $(".totalframes").empty().append("<p>Total Frames: " + characterArr["0"].attacks.jab["0"].total_frames+ "  Active Frames: " + characterArr[0].attacks.jab[0].hit_start + "-" +characterArr[0].attacks.jab[0].hit_end + "<p>");
+  $(".totalframes").empty().append("<p>Total Frames: " + characterArr["0"].attacks.jab["0"].total_frames+ "<p> <p>  Active Frames: " + characterArr[0].attacks.jab[0].hit_start + "-" +characterArr[0].attacks.jab[0].hit_end + "<p>");
   $(".card-title").empty().append("Frame Data for " + $(this).text())
-  $(".gifattack").attr("src", "https://smashboards.com/proxy.php?image=http%3A%2F%2Fi882.photobucket.com%2Falbums%2Fac30%2FX1-12%2FFoxJab.gif%3Ft%3D1282910671&hash=26b37c01a59c854fece263e44035a494")
+  $('.gifattack').attr("src", charactergif.jabs[0].gif);
+  // $(".gifattack")[0].load()
   // $(".activeframes").append("<p>Active Frames: " + foxArr.hit_start + "-" +foxArr.hit_end+ "<p>" )
   }
 )
 
 $(".jab2").click(function(){
-  $(".totalframes").empty().append("<p>Total Frames: " + characterArr["0"].attacks.jab["1"].total_frames+ "  Active Frames: " + characterArr[0].attacks.jab[1].active_start + "-" +characterArr[0].attacks.jab[1].active_end);
-  $(".card-title").empty().append("Frame Data for " + $(this).text())
+  $(".totalframes").empty().append("<p>Total Frames: " + characterArr["0"].attacks.jab["1"].total_frames+ "<p> <p>  Active Frames: " + characterArr[0].attacks.jab[1].active_start + "-" +characterArr[0].attacks.jab[1].active_end);
+  $(".card-title").empty().append("Frame Data for " + $(this).text());
+  $('.gifattack').attr("src", charactergif.jabs[1].gif);
   // $(".gifattack").attr("src", "https://smashboards.com/proxy.php?image=http%3A%2F%2Fi882.photobucket.com%2Falbums%2Fac30%2FX1-12%2FFoxJab.gif%3Ft%3D1282910671&hash=26b37c01a59c854fece263e44035a494")
   // $(".activeframes").append("<p>Active Frames: " + foxArr.hit_start + "-" +foxArr.hit_end+ "<p>" )
   }
 )
 
 $(".jab3").click(function(){
-  $(".totalframes").empty().append("<p>Total Frames: " + characterArr["0"].attacks.jab["2"].total_frames+ "  Active Frames: " + characterArr[0].attacks.jab[2].hit_start + "-" +characterArr[0].attacks.jab[2].hit_end + "<p>");
-  $(".card-title").empty().append("Frame Data for" + $(this).text())
+  $(".totalframes").empty().append("<p>Total Frames: " + characterArr["0"].attacks.jab["2"].total_frames+ "<p> <p>  Active Frames: " + characterArr[0].attacks.jab[2].hit_start + "-" +characterArr[0].attacks.jab[2].hit_end + "<p>");
+  $(".card-title").empty().append("Frame Data for" + $(this).text());
+  $('.gifattack').attr("src", charactergif.jabs[2].gif);
 })
 //Tilt functions
 $(".ftilt").click(function(){
-  $(".totalframes").empty().append("<p>Total Frames: " + characterArr["0"].attacks.tilt[2].total_frames+ "  Active Frames: " + characterArr["0"].attacks.tilt[2].hit_start + "-" +characterArr["0"].attacks.tilt[2].hit_end + "<p>");
-  $(".card-title").empty().append("Frame Data for " + $(this).text())
+  $(".totalframes").empty().append("<p>Total Frames: " + characterArr["0"].attacks.tilt[2].total_frames+ " <p> <p> Active Frames: " + characterArr["0"].attacks.tilt[2].hit_start + "-" +characterArr["0"].attacks.tilt[2].hit_end + "<p>");
+  $(".card-title").empty().append("Frame Data for " + $(this).text());
+  $('.gifattack').attr("src", charactergif.tilts[1].gif);
   }
 )
 
 $(".uptilt").click(function(){
-  $(".totalframes").empty().append("<p>Total Frames: " + characterArr["0"].attacks.tilt[3].total_frames+ "  Active Frames: " + characterArr["0"].attacks.tilt[4].hit_start + "-" +characterArr["0"].attacks.tilt[4].hit_end + "<p>");
-  $(".card-title").empty().append("Frame Data for " + $(this).text())
+  $(".totalframes").empty().append("<p>Total Frames: " + characterArr["0"].attacks.tilt[3].total_frames+ "<p> <p> Active Frames: " + characterArr["0"].attacks.tilt[4].hit_start + "-" +characterArr["0"].attacks.tilt[4].hit_end + "<p>");
+  $(".card-title").empty().append("Frame Data for " + $(this).text());
+  $('.gifattack').attr("src", charactergif.tilts[4].gif);
   }
 )
 
 $(".dtilt").click(function(){
-  $(".totalframes").empty().append("<p>Total Frames: " + characterArr["0"].attacks.tilt[2].total_frames+ "  Active Frames: " + characterArr["0"].attacks.tilt[3].hit_start + "-" +characterArr["0"].attacks.tilt[3].hit_end + "<p>");
-  $(".card-title").empty().append("Frame Data for " + $(this).text())
+  $(".totalframes").empty().append("<p>Total Frames: " + characterArr["0"].attacks.tilt[2].total_frames+ " <p> <p>Active Frames: " + characterArr["0"].attacks.tilt[3].hit_start + "-" +characterArr["0"].attacks.tilt[3].hit_end + "<p>");
+  $(".card-title").empty().append("Frame Data for " + $(this).text());
+  $('.gifattack').attr("src", charactergif.tilts[0].gif);
   }
 )
 // Aerial attack functions. Changes depends on times hit
@@ -58,30 +91,76 @@ $(".upair").click(function(){
   if (characterArr["0"].id == 6) {
     $(".totalframes").empty().append("<p>Total Frames: " + characterArr["0"].attacks.aerial[1].total_frames+ "<p> <p>  Active Frames:<p>  <p>First Hit: "  + characterArr["0"].attacks.aerial[1].hit_start + "-" +characterArr["0"].attacks.aerial[1].hit_end
     + "<p> <p>Second hit: " +characterArr["0"].attacks.aerial[1].hit_second_start + "-" + characterArr["0"].attacks.aerial[1].hit_second_end +"<p>");
-    $(".card-title").empty().append("Frame Data for " + $(this).text())
+    $(".card-title").empty().append("Frame Data for " + $(this).text());
+    $('.gifattack').attr("src", charactergif.aerials[4].gif);
   } else {
-    $(".totalframes").empty().append("<p>Total Frames: " + characterArr["0"].attacks.aerial[1].total_frames+ "  Active Frames: "  + characterArr["0"].attacks.aerial[1].hit_start + "-" +characterArr["0"].attacks.aerial[1].hit_end + "<p>")
-    $(".card-title").empty().append("Frame Data for " + $(this).text())
+    $(".totalframes").empty().append("<p>Total Frames: " + characterArr["0"].attacks.aerial[1].total_frames+ "<p> <p>  Active Frames: "  + characterArr["0"].attacks.aerial[1].hit_start + "-" +characterArr["0"].attacks.aerial[1].hit_end + "<p>")
+    $(".card-title").empty().append("Frame Data for " + $(this).text());
+    $('.gifattack').attr("src", charactergif.aerials[4].gif);
   }
 })
 
 $(".nair").click(function(){
-  $(".totalframes").empty().append("<p>Total Frames: " + characterArr["0"].attacks.aerial["0"].total_frames+ "  Active Frames: " + characterArr["0"].attacks.aerial[0].hit_start + "-" +characterArr["0"].attacks.aerial[0].hit_end + "<p>");
-  $(".card-title").empty().append("Frame Data for " + $(this).text()+":")
+  $(".totalframes").empty().append("<p>Total Frames: " + characterArr["0"].attacks.aerial["0"].total_frames+ "<p> <p>  Active Frames: " + characterArr["0"].attacks.aerial[0].hit_start + "-" +characterArr["0"].attacks.aerial[0].hit_end + "<p>");
+  $(".card-title").empty().append("Frame Data for " + $(this).text()+":");
+  $('.gifattack').attr("src", charactergif.aerials[3].gif);
   }
 )
 
 $(".bair").click(function(){
-  $(".totalframes").empty().append("<p>Total Frames: " + characterArr["0"].attacks.aerial[4].total_frames+ "  Active Frames: " + characterArr["0"].attacks.aerial[4].hit_start + "-" +characterArr["0"].attacks.aerial[4].hit_end +"<p>");
-  $(".card-title").empty().append("Frame Data for " + $(this).text())
+  $(".totalframes").empty().append("<p>Total Frames: " + characterArr["0"].attacks.aerial[4].total_frames+ "<p> <p>  Active Frames: " + characterArr["0"].attacks.aerial[4].hit_start + "-" +characterArr["0"].attacks.aerial[4].hit_end +"<p>");
+  $(".card-title").empty().append("Frame Data for " + $(this).text());
+  $('.gifattack').attr("src", charactergif.aerials[0].gif);
+  }
+)
+
+$(".fair").click(function(){
+  $(".totalframes").empty().append("<p>Total Frames: " + characterArr[0].attacks.aerial[3].total_frames+ "<p> <p>  Active Frames: " + characterArr["0"].attacks.aerial[3].hit_start + "-" +characterArr["0"].attacks.aerial[3].hit_end +"<p>");
+  $(".card-title").empty().append("Frame Data for " + $(this).text());
+  $('.gifattack').attr("src", charactergif.aerials[2].gif);
+
   }
 )
 
 
+$(".dair").click(function(){
+  $(".totalframes").empty().append("<p>Total Frames: " + characterArr[0].attacks.aerial[2].total_frames+ "<p> <p>  Active Frames: " + characterArr["0"].attacks.aerial[2].hit_start + "-" +characterArr["0"].attacks.aerial[2].hit_end +"<p>");
+  $(".card-title").empty().append("Frame Data for " + $(this).text());
+  $('.gifattack').attr("src", charactergif.aerials[1].gif);
+
+  }
+)
+
+$(".fsmash").click(function(){
+  $(".totalframes").empty().append("<p>Total Frames: " + characterArr[0].attacks.smash[0].total_frames+ "<p> <p>  Active Frames: " + characterArr["0"].attacks.smash[0].hit_start + "-" +characterArr["0"].attacks.smash[0].hit_end +"<p>");
+  $(".card-title").empty().append("Frame Data for " + $(this).text());
+  $('.gifattack').attr("src", charactergif.smashAttacks[1].gif);
+
+  }
+)
+
+$(".dsmash").click(function(){
+  $(".totalframes").empty().append("<p>Total Frames: " + characterArr[0].attacks.smash[2].total_frames+ "<p> <p>  Active Frames: " + characterArr["0"].attacks.aerial[2].hit_start + "-" +characterArr["0"].attacks.aerial[2].hit_end +"<p>");
+  $(".card-title").empty().append("Frame Data for " + $(this).text());
+  $('.gifattack').attr("src", charactergif.smashAttacks[0].gif);
+
+  }
+)
+
+$(".upsmash").click(function(){
+  $(".totalframes").empty().append("<p>Total Frames: " + characterArr[0].attacks.smash[1].total_frames+ "<p> <p> Active Frames: " + characterArr["0"].attacks.smash[1].hit_start + "-" +characterArr["0"].attacks.smash[1].hit_end +"<p>");
+  $(".card-title").empty().append("Frame Data for " + $(this).text());
+  $('.gifattack').attr("src", charactergif.smashAttacks[2].gif);
+
+  }
+)
+
+
+
 //This bracket is appReadybracket!
+})
+
 }
-
-
   //click fox, get fox attack data, put into var.
 //   var foxArr = []
 //   $(".fox").click(function() {
